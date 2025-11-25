d7f25f01c926869056e37b8fde940986e37c3a76
Beanie <K00281273@student.tus.ie>
2025-11-25 19:14:45 +0000

    Update EnemyhitBox.cs

--- Assets/Scripts/EnemyhitBox.cs
+++ Assets/Scripts/EnemyhitBox.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class EnemyhitBox : MonoBehaviour
 {
+    public Animator animator;
     public GameObject hitbox;
+    public float repeatPunchTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         hitbox.transform.position = new Vector3(0,12,0);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       repeatPunchTime += Time.deltaTime;
+        if (repeatPunchTime > 5)
+        {
+            animator.SetTrigger("EnemyAttack");
+            repeatPunchTime = 0;
+        }
     }
 
     public void MoveHitbox()
