e5ed2845b5c0813541f38645abec56ac787914ff
Beanie <K00281273@student.tus.ie>
2026-01-21 17:35:46 +0000

    jab yayyy

---
+++ Assets/Scripts/TutorBot Scripts/TBotAttackScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TBotAttackScript : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
