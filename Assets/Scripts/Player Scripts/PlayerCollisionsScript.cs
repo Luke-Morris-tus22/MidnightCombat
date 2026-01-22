cddfa18e5b8f87556d5a2149c3be86e76f812716
Beanie <K00281273@student.tus.ie>
2026-01-22 17:56:02 +0000

    bot jab fully functional

--- Assets/Scripts/Player Scripts/PlayerCollisionsScript.cs
+++ Assets/Scripts/Player Scripts/PlayerCollisionsScript.cs
@@ -4,16 +4,20 @@ public class PlayerCollisionsScript : MonoBehaviour
 {
     private Animator _animator;
     private Collider2D _collisionCollider;
+    public HealthBarScript HealthBarScript;
+    public float timeSinceDamageTaken;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _collisionCollider = GetComponent<Collider2D>();
+        _animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _animator = GetComponent<Animator>();   
+        timeSinceDamageTaken += Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,8 +25,33 @@ public class PlayerCollisionsScript : MonoBehaviour
       //  Debug.Log("Trigger");
         if (other.CompareTag("EnemyAttack"))
         {
-            Debug.Log("Player Hit");
-            _animator.SetTrigger("HitRight");
+            HurtBoxScript hurtBoxScript = other.GetComponent<HurtBoxScript>();
+            if (hurtBoxScript.isRight)
+            {
+                _animator.SetTrigger("HitRight");
+
+            }
+            else
+            {
+                _animator.SetTrigger("HitLeft");
+            }
+            HealthBarScript.TakesDamage(hurtBoxScript.damage);
+            timeSinceDamageTaken = 0;
+        }
+    }
+
+    public bool playerDodgedCheck()
+    {
+        //Debug.Log("dodge check");
+        if (timeSinceDamageTaken > 2)
+        {
+           // Debug.Log("dodge check true");
+            return true;
+        }
+        else
+        {
+           // Debug.Log("dodge check false");
+            return false;
         }
     }
 }
--- Assets/Scripts/TutorBot Scripts/TBotAttackScript.cs
+++ Assets/Scripts/TutorBot Scripts/TBotAttackScript.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 public class TBotAttackScript : MonoBehaviour
 {
     private Animator _animator;
-    public float attackInterval = 3; //seconds before starting an attack
+    public float attackInterval = 5; //seconds before starting an attack
     private float _attackIntervalCounter;
-
+    private TBotHurtScript _botHurtScript;
+    public PlayerCollisionsScript playerCollisionsScript;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _botHurtScript = GetComponent<TBotHurtScript>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class TBotAttackScript : MonoBehaviour
         {
             Attack();
         }
+        //Debug.Log(_attackIntervalCounter);
     }
 
     public void restartAttackCounter()
@@ -32,5 +35,22 @@ public class TBotAttackScript : MonoBehaviour
     public void Attack()
     {
         _animator.SetTrigger("StartJab");
+        _attackIntervalCounter = 0;
+    }
+
+    public void attackSuccessCheck()
+    {
+        //Debug.Log("attack success check");
+        _animator.SetBool("MissedAttack", playerCollisionsScript.playerDodgedCheck());
+    }
+
+    public void LowerGuard()
+    {
+        _botHurtScript.guardUp = false;
+    }
+
+    public void RaiseGuard()
+    {
+        _botHurtScript.guardUp = true;
     }
 }
